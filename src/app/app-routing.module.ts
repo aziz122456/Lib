f014e971b6b11479ae83699438773cdd3e0ec136
Lunaria <AhmedBrahimMekni@proton.me>
2025-01-10 09:23:18 +0100

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,17 @@ import { BookAddComponent } from './components/book-add/book-add.component';
 import { BookDeleteComponent } from './components/book-delete/book-delete.component';
 import { CartListComponent } from './components/cart-list/cart-list.component';
 import { OrderListComponent } from './components/order-list/order-list.component';
+import { UserComponent } from './components/user/user.component';
+
 const routes: Routes = [
   { path: 'test', component: TestComponent },
   { path: 'book-search', component: BookSearchComponent },
   { path: 'book-add', component: BookAddComponent },
   { path: 'book-delete', component: BookDeleteComponent },
   { path: 'cart-list', component: CartListComponent },
+  { path: 'cart-list', component: CartListComponent },
+  { path: 'user', component: UserComponent },
+
   { path: 'order-list', component: OrderListComponent },
   { path: '', redirectTo: '/test', pathMatch: 'full' }, // Redirect to Test Component
 ];
